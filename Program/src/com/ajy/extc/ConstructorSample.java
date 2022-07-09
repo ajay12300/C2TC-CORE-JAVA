8ee89c88965948b198d77345ed99ff6a4659c0bf
Ajay Yadav <ajayyadav7860.ay@gmail.com>
2022-07-09 13:24:30 +0530

    update

---
+++ Program/src/com/ajy/extc/ConstructorSample.java
@@ -0,0 +1,28 @@
+package com.ajy.extc;
+
+
+class ConstructorSample {
+
+	ConstructorSample() {
+		
+		System.out.println("I am Constructor");
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ConstructorSample obj1= new ConstructorSample();
+		ConstructorSample obj2= new ConstructorSample();
+		ConstructorSample obj3= new ConstructorSample();
+		ConstructorSample obj4= new ConstructorSample();
+		ConstructorSample obj5= new ConstructorSample();
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Program/src/com/ajy/extc/ContinueDemo.java
@@ -0,0 +1,22 @@
+package com.ajy.extc;
+
+import mumbai.*;
+public class ContinueDemo 
+{
+
+	public static void main(String[] args) 
+	{
+	int x=0;
+	do
+	{	x++;
+		System.out.println(x);
+			if(++x<5)
+				continue;
+			x++;
+			System.out.println(x);
+	
+		} while(++x<10);
+	}
+}
+
+
