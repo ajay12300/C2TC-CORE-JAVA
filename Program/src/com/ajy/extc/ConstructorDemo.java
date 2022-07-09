eb140a61b2a855005d1a1d3957451feaf19fe80a
Ajay Yadav <ajayyadav7860.ay@gmail.com>
2022-07-09 13:13:04 +0530

    update

--- Program/src/com/ajy/extc/ConstructorDemo.java
+++ Program/src/com/ajy/extc/ConstructorDemo.java
@@ -1,11 +1,11 @@
 package com.ajy.extc;
 
-public class Constructor {
+public class ConstructorDemo {
 	
 	String name;
 	int roll;
 	
-	Constructor(String name, int roll){
+	ConstructorDemo(String name, int roll){
 		
 		this.name=name;
 		this.roll=roll;
@@ -13,8 +13,8 @@ public class Constructor {
 
 	public static void main(String[] args)
 	{
-		Constructor s1=new Constructor("Ajay",20);
-		Constructor s2=new Constructor("Rahul",30);
+		ConstructorDemo s1=new ConstructorDemo("Ajay",20);
+		ConstructorDemo s2=new ConstructorDemo("Rahul",30);
 		
 		System.out.println(s1.name+" "+s1.roll);
 		System.out.println(s2.name+" "+s2.roll);
